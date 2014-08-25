8f0792711d32103cb8c27b8b304e0557382776ab
Mathias Fussenegger <f.mathias@zignar.net>
2014-08-25 17:13:02 +0200

    Added the possibility to decommission nodes and stop them gracefully.

--- src/main/java/org/elasticsearch/indices/cluster/IndicesClusterStateService.java
+++ src/main/java/org/elasticsearch/indices/cluster/IndicesClusterStateService.java
@@ -149,7 +149,7 @@ public class IndicesClusterStateService extends AbstractLifecycleComponent<Indic
             return;
         }
 
-        if (!lifecycle.started()) {
+        if (!(lifecycle.started() || lifecycle.disabled())) {
             return;
         }
 
