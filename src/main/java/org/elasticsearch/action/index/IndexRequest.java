3797925bc913be59c51ff9d51716f84e159b6c95
dobe <bernddorn@gmail.com>
2015-02-18 15:26:09 +0100

    Route empty strings by the hash of an empty string instead of treating them as null. This reverts f4adff652caa2725d0f7b21e32055827ee3b375a which was discussed at https://github.com/elasticsearch/elasticsearch/issues/1450

--- src/main/java/org/elasticsearch/action/index/IndexRequest.java
+++ src/main/java/org/elasticsearch/action/index/IndexRequest.java
@@ -265,11 +265,7 @@ public class IndexRequest extends ShardReplicationOperationRequest<IndexRequest>
      * and not the id.
      */
     public IndexRequest routing(String routing) {
-        if (routing != null && routing.length() == 0) {
-            this.routing = null;
-        } else {
-            this.routing = routing;
-        }
+        this.routing = routing;
         return this;
     }
 
