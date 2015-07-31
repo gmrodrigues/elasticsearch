27db1cb7759b56ab46ae7d3ca4810f52106c7275
Christian Haudum <christian.haudum@gmail.com>
2015-07-31 08:23:31 +0200

    removed assertion that futures are not executing on transport thread
    required for blob transport

--- src/main/java/org/elasticsearch/common/util/concurrent/BaseFuture.java
+++ src/main/java/org/elasticsearch/common/util/concurrent/BaseFuture.java
@@ -113,7 +113,6 @@ public abstract class BaseFuture<V> implements Future<V> {
      */
     @Override
     public V get() throws InterruptedException, ExecutionException {
-        assert Transports.assertNotTransportThread("Blocking operation");
         // Transports.assertNotTransportThread("Blocking operation");
         return sync.get();
     }
